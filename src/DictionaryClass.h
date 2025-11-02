4cb5174fee394b1f3f889f7aaa8450e9742225f4
Jessica D <154709968+jessicadavies2003@users.noreply.github.com>
2025-11-02 00:38:41 +0000

    Update DictionaryClass.h

--- src/DictionaryClass.h
+++ src/DictionaryClass.h
@@ -1,9 +1,86 @@
+#pragma once
+
 #include <string>
 #include <vector>
 using namespace std;
 
 // desc to be added
-class Dictionary {
+class StrVecDictionary {
+private:
+	vector<string> keys;
+	vector<vector<string>> values;
+	int indexOf(string key) {
+		for (int i = 0; i < keys.size(); i++) {
+			if (keys[i] == key) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+public:
+	StrVecDictionary() {
+
+	}
+
+	// appends a given key-value pair to the dictionary. true if done, false if not.
+	bool addKeyValue(string key, vector<string> value) {
+		if (indexOf(key) == -1) {
+			keys.push_back(key);
+			values.push_back(value);
+			return true;
+		}
+		return false;
+	}
+
+	// returns the key at a given index.
+	string key(int index) {
+		return keys[index];
+	}
+
+	// returns the value with a given key.
+	vector<string> value(string key) {
+		if (indexOf(key) == -1) {
+			return {};
+		}
+		else {
+			return values[indexOf(key)];
+		}
+	}
+
+	// returns a vector of all keys in the dictionary
+	vector<string> getKeys() {
+		return keys;
+	}
+
+	// returns a vector of all values in the dictionary
+	vector<vector<string>> getValues() {
+		return values;
+	}
+
+	// removes a key-value pair given the key value.
+	void removeKeyValue(string key) {
+		int idx = indexOf(key);
+
+		if (idx == 0) {
+			keys.erase(keys.begin());
+			values.erase(values.begin());
+		}
+		else {
+			keys.erase(keys.begin() + idx);
+			values.erase(values.begin() + idx);
+		}
+	}
+
+	// removes the last key-value pair inserted.
+	void popItem() {
+		keys.erase(keys.end());
+		values.erase(values.end());
+	}
+};
+
+// desc to be added
+class StrStrDictionary {
 private:
 	vector<string> keys;
 	vector<string> values;
@@ -17,8 +94,8 @@ private:
 	}
 
 public:
-	Dictionary() {
-		
+	StrStrDictionary() {
+
 	}
 
 	// appends a given key-value pair to the dictionary.
