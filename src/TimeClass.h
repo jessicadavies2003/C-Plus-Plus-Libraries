a06c97dd6804bf49b306a3fa227acacf4e294ba5
Jessica D <154709968+jessicadavies2003@users.noreply.github.com>
2025-11-14 19:34:55 +0000

    Update TimeClass.h

--- src/TimeClass.h
+++ src/TimeClass.h
@@ -7,25 +7,12 @@
 // a c++ library for all things time related.
 class TimePlusPlus {
 private:
+	std::vector<std::string> months = { "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec" };
+	std::vector<int> numDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	time_t timeObj;
 	int timeInSeconds; // since Jan 1 1970 00:00
-
 	int startingYear = 1970;
-
-	int storedYear;
-	int storedMonth;
-	int storedDate;
-	int storedHour;
-	int storedMinute;
-	int storedSeconds;
-
-	void displayIntVector(std::vector<int> arr) {
-		cout << "{ ";
-		for (int i = 0; i < arr.size(); i++) {
-			cout << arr[i] << ", ";
-		}
-		cout << "}" << endl;
-	}
 
 	int numYears2Seconds(int numYears, int leapYearStartIdx = 3) {
 		if (numYears == 1) {
@@ -55,7 +42,56 @@ private:
 		return result;
 	}
 
+	std::vector<int> findSubStrings(std::string s, std::string sub) {
+		std::vector<int> allOccurences;
+		std::string temp = s;
+
+		while (temp.find(sub) != -1) {
+			int idx = temp.find(sub);
+			allOccurences.push_back(temp.find(sub));
+			temp.erase(idx, sub.size());
+		}
+
+		return allOccurences;
+	}
+
+	// replaces a substring with a new substring.
+	std::string replaceSubstr(std::string s, std::string oldSubStr, std::string newSubStr) {
+		std::vector<int> allOccurences = findSubStrings(s, oldSubStr);
+		std::string result = s;
+		int len = oldSubStr.size();
+
+		for (int index : allOccurences) {
+			result.replace(index, len, newSubStr);
+		}
+
+		return result;
+	}
+
+	std::vector<int> getLeapYears(int year1, int year2) {
+		std::vector<int> leapYears;
+		for (int i = year1; i < year2 + 1; i++) {
+			if (i % 4 == 0) {
+				leapYears.push_back(i);
+			}
+		}
+		return leapYears;
+	}
+
 public:
+	int storedYear;
+	int storedMonth;
+	int storedDate;
+	int storedHour;
+	int storedMinute;
+	int storedSeconds;
+
+	TimePlusPlus(int seconds) {
+		time_t t = seconds;
+		timeObj = t;
+		timeInSeconds = (int)timeObj;
+	}
+
 	// stores the current time
 	TimePlusPlus() {
 		timeObj = time(NULL);
@@ -68,7 +104,29 @@ public:
 	}
 
 	time_t dateToTimeObj(int year, int month, int day, int hour = 0, int minute = 0, int seconds = 0) {
+		int numSeconds = 0;
 
+		// years
+		for (int i = 1970; i < year + 1; i++) {
+			if (i % 4 == 0) {
+				numSeconds += (86400 * 366);
+			}
+			else {
+				numSeconds += (86400 * 365);
+			}
+		}
+
+		// months
+		//numSeconds += (numDays[month-1] * 86400);
+
+		// days
+		//numSeconds += (day * 86400);
+
+		// hrs, mins, seconds
+		numSeconds += (seconds + (minute * 60) + (minute * (60 * 60)));
+
+		time_t t = numSeconds;
+		return t;
 	}
 
 	TimePlusPlus(int year, int month, int day, int hour = 0, int minute = 0, int seconds = 0) {
@@ -135,5 +193,23 @@ public:
 		}
 
 		return numYears;
+	}
+
+	bool operator>(TimePlusPlus dateObj) {
+		return (this->timetObjToSeconds() > dateObj.timetObjToSeconds());
+	}
+
+	int calculateDurationInSeconds(TimePlusPlus dateObj) {
+		int calculatedSeconds;
+		TimePlusPlus &currTimeObj = *this;
+
+		if (currTimeObj > dateObj) {
+			calculatedSeconds = (currTimeObj.timetObjToSeconds() - dateObj.timetObjToSeconds());
+		}
+		else {
+			calculatedSeconds = (dateObj.timetObjToSeconds() - currTimeObj.timetObjToSeconds());
+		}
+
+		return calculatedSeconds;
 	}
 };
